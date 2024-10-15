75b2628d5f2e0f5e334176f010f8a079092917c0
Nekrasov Vadim <vadim65337.68@gmail.com>
2024-10-15 14:29:01 +0600

    Prepared an MVC application. Added a Product model.

---
+++ Shop/Shop/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Shop.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime DateCreation { get; set; }
+    public DateTime DateUpdate { get; set; }
+    public string Image { get; set; }
+    public int Price { get; set; }
+}
---
+++ Shop/Shop/Models/ProductContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Shop.Models;
+
+public class ProductContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    
+    public ProductContext(DbContextOptions<ProductContext> options) : base(options) {}
+}
